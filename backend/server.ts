2cb7b5d37b1d26079f1c4fc3951af7ec3065f3e6
egwolk <erincovacha@gmail.com>
2025-04-28 12:15:43 +0800

    feat: add CORS middleware to server for handling cross-origin requests

--- backend/server.ts
+++ backend/server.ts
@@ -1,8 +1,16 @@
 import { Hono } from "@hono/hono";
 import { noteRoutes } from "./routes/note.route.ts";
+import { cors } from "@hono/hono/cors";
 
 const app = new Hono();
 
+app.use("*", cors({
+    origin: "http://127.0.0.1:5173",
+    allowMethods: ["GET", "POST", "PUT", "DELETE","OPTIONS"],
+    allowHeaders: ["Content-Type"],
+    maxAge: 600,
+})
+);
 noteRoutes(app);
 
 Deno.serve(app.fetch)
