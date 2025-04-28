b6bb5eecea51fbad7e0d03882a05d9bf62ae4ad1
egwolk <erincovacha@gmail.com>
2025-04-28 12:16:17 +0800

    feat: add route for retrieving all notes

--- backend/routes/note.route.ts
+++ backend/routes/note.route.ts
@@ -3,6 +3,7 @@ import * as NoteController from "../controllers/note.controller.ts";
 
 export const noteRoutes = (app: Hono) => {
     app.post('/notes', NoteController.addNote);
+    app.get('/notes', NoteController.getAllNotes);
     app.get('/notes/:id', NoteController.getNote);
     app.put('/notes/:id', NoteController.updateNote);
     app.delete('/notes/:id', NoteController.deleteNote);
